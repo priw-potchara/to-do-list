e2a119951a95c407e833bccac803dac89c3be809
“potchara.t” <potchara.t@gfinthailand.com>
2024-05-25 00:10:56 +0700

    [update] api

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const app = express()
+const bodyParser = require('body-parser')
 
-const routeToDoList = require('./to-do-list-group');
+const routeToDoListGroup = require('./to-do-list-group');
+const routeToDoList = require('./to-do-list');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
-routeToDoList(app);
+routeToDoListGroup(app);
+routeToDoList(app)
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Start server at port ${port}`)
 })
